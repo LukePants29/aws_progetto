2b2574f47aca92ebb28dd2ecab9cb47485565d0e
LukePants29 <lucapanta29@gmail.com>
2023-04-11 16:54:23 +0200

    login mod

--- login_form.php
+++ login_form.php
@@ -12,7 +12,7 @@
         <h1>Login Utente</h1>
         <form action="login.php" method="post">
             <label for="username">Username:</label>
-            <input type="text" name="name" maxlength="100" placeholder="DanielePredello-RubenScopacasa" require>
+            <input type="text" name="nickname" maxlength="100" placeholder="DanielePredello-RubenScopacasa" require>
             <label for="password">Password:</label>
             <input type="password" name="password" maxlength="100" placeholder="12345678-87654321" require>
             <button type="submit">Accedi</button>
